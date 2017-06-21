30b4a73638c02291fc62f02682ee89c7a951736e
Jakob Kosir <kosir.jakob@gmail.com>
2017-06-21 17:58:16 +0200

    Inital with yo typescript + livereload

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require('gulp');
+var tsc = require('gulp-tsc');
+var shell = require('gulp-shell');
+var livereload = require('gulp-livereload');
+var tslint = require('gulp-tslint');
+var inject = require('gulp-inject');
+var es = require('event-stream');
+var webserver = require('gulp-webserver');
+
+var paths = {
+  tscripts: {
+    src: ['app/src/**/*.ts'],
+    dest: 'app/build'
+  },
+  all_src: 'app/src/**/*.*'
+};
+
+gulp.task('default', ['build', 'webserver', 'watch']);
+
+// ** Running ** //
+
+
+// ** Watching ** //
+
+gulp.task('watch', function () {
+  livereload.listen();
+  gulp.watch(paths.all_src, ['build']);
+});
+
+// ** Compilation ** //
+
+gulp.task('build', function () {
+
+  var ts = gulp
+    .src(paths.tscripts.src)
+    .pipe(tsc({
+      module: "commonjs",
+      emitError: false
+    }));
+
+  var css = gulp.src(['./app/src/**/*.css'], {read: false});
+
+  return gulp.src('./app/src/index.html')
+    .pipe(inject(es.merge(ts, css), {ignorePath: '/'}))
+    .pipe(gulp.dest('./app/build'))
+    .pipe(livereload());
+});
+
+// ** Linting ** //
+
+gulp.task('lint', ['lint:default']);
+gulp.task('lint:default', function () {
+  return gulp.src(paths.tscripts.src)
+    .pipe(tslint())
+    .pipe(tslint.report('prose', {
+      emitError: false
+    }));
+});
+
+/* Server */
+
+gulp.task('webserver', function() {
+  gulp.src('./app/build')
+    .pipe(webserver({
+      livereload: true,
+      open: true
+    }));
+});
