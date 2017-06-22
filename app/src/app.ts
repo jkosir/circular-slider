44131f64f6fb1ea81e3d210e53082833e08bcccb
Jakob Košir <kosir.jakob@gmail.com>
2017-06-22 20:55:54 +0200

    Wheel arc.

--- app/src/app.ts
+++ app/src/app.ts
@@ -9,6 +9,7 @@ interface Options {
 class Slider {
   container: HTMLElement;
   handle: HTMLElement;
+  arc: HTMLElement;
   options: Options;
   mousedown: boolean;
   wheelBounds: ClientRect;
@@ -29,7 +30,8 @@ class Slider {
     this.handleBounds = this.handle.getBoundingClientRect();
     this.mousedown = false;
 
-    this.setListeners()
+    this.arc = <HTMLElement>this.container.getElementsByClassName('wheel-progress-fill')[0];
+    this.setListeners();
 
   }
 
@@ -49,8 +51,49 @@ class Slider {
       this.handle.style.left = Math.cos(rad) * radius_minus_handle + (this.wheelBounds.width / 2) + 'px';
       this.handle.style.top = Math.sin(rad) * radius_minus_handle + (this.wheelBounds.height / 2) + 'px';
 
+      let deg = rad * (180 / Math.PI);
+      // Convert radians to degrees relative to positive y-axis
+      if (deg <= 0 && deg >= -90) {
+        deg = 90 + deg;
+      } else if (deg < -90) {
+        deg = 270 + 180 + deg;
+      } else {
+        deg += 90;
+      }
 
+      this.arc.setAttribute('d', this.describeArc(
+        100,
+        100,
+        90,
+        0,
+        deg
+      ));
     })
+  }
+
+  polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+    let angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
+
+    return {
+      x: centerX + (radius * Math.cos(angleInRadians)),
+      y: centerY + (radius * Math.sin(angleInRadians))
+    };
+  }
+
+  // Ref: https://stackoverflow.com/questions/5736398/how-to-calculate-the-svg-path-for-an-arc-of-a-circle
+  // Or we could use d3...
+  describeArc(x, y, radius, startAngle, endAngle) {
+
+    let start = this.polarToCartesian(x, y, radius, endAngle);
+    let end = this.polarToCartesian(x, y, radius, startAngle);
+
+    let largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+
+    return [
+      "M", start.x, start.y,
+      "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
+    ].join(" ");
+
   }
 }
 
